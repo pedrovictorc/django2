2e21c09023500cfbffc2df8cbcf9138997c9311c
pedrovictorrc <pedrocarvalhophb@gmail.com>
2020-07-02 10:29:05 -0300

    Projeto Finalizado

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import index, produto, contato
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contato/', contato, name='contato'),
+    path('produto/', produto, name='produto'),
+]
